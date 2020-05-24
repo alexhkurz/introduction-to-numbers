353d9b2d9c2d253688de41987831fac50a33b586
alexhkurz <alexhkurz@gmail.com>
2020-05-23 20:26:11 -0700

    groupokus

---
+++ src/groupoku.py
@@ -0,0 +1,22 @@
+"""
+Fill in the table below
+Run this program with `python groupoku.py`
+"""
+
+import Group
+
+carrier = [0,1,2,3]
+
+table = [
+    [0,1,2,3],
+    [1,2,3,0],
+    [2,3,0,1],
+    [3,0,1,2],
+]
+
+def op(x,y):
+    return table[x][y]
+
+Group.__init__(carrier,0,op)
+Group.print_table(table)
+Group.test_group()
