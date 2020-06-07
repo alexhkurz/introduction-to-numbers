8d55130243f561bbe893fd3c6e823170d742801f
alexhkurz <alexhkurz@gmail.com>
2020-06-07 07:43:18 -0700

    modified plot1.py

--- src/plot1.py
+++ src/plot1.py
@@ -4,10 +4,8 @@ import numpy as np
 # 100 linearly spaced numbers
 x = np.linspace(-5,5,100)
 
-# the function, which is y = x^2 here
-y = x**2 + 2*x - 1
-
-# setting the axes at the centre
+# the function
+y = x**2
 
 # plot the function
 plt.plot(x,y, 'r')
