1346c82f0d41b5ffca4264d97e9806bc0c8e98d5
alexhkurz <alexhkurz@gmail.com>
2020-06-13 17:55:55 -0700

    plot3.py added

--- src/plot2.py
+++ src/plot2.py
@@ -16,6 +16,10 @@ ax.spines['bottom'].set_position('zero')
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 
+# making a tick for every unit
+plt.xticks(np.arange(min(x), max(x)+1, 1))
+plt.yticks(np.arange(min(y1), max(y1)+1, 2))
+
 # plot the function
 plt.plot(x,y1, 'r')
 plt.plot(x,y2, 'b')
